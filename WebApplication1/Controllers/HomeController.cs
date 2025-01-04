5d6a4a9c21605d25be432304aca98ca0c9bcd5d2
Mahdi <137503197+Mahdi-Yadi@users.noreply.github.com>
2025-01-04 20:02:00 +0330

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(string email,string subject,string text)
+        {
+
+            var res = MailServices.MailSender.SendMail(email, subject,text);
+
+            if (res == true)
+            {
+                ViewBag.result = "ایمیل ارسال شد";
+                return Redirect("/");
+            }
+            else
+            {
+                ViewBag.result = "ایمیل ارسال نشد";
+                return Redirect("/");
+            }
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/MailServices/MailSender.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Mail;
+namespace WebApplication1.MailServices;
+public class MailSender
+{
+
+    public static bool SendMail(string emailAddress, string subject, string body)
+    {
+        var res = false;
+
+        MailMessage mail = new MailMessage();
+
+        SmtpClient smtp = new SmtpClient("mail.example.org");
+
+        if (!res)
+        {
+            mail.From = new MailAddress("info@example.org", "title");
+
+            mail.To.Add(emailAddress);
+
+            mail.Subject = subject;
+
+            mail.Body = body;
+
+            mail.IsBodyHtml = true;
+
+            smtp.Port = 587;
+
+            smtp.EnableSsl = true;
+
+            smtp.Credentials = new NetworkCredential("info@example.org", "password");
+
+            smtp.Send(mail);
+
+            res = true;
+        }
+
+        return res;
+    }
+
+}
